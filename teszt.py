d73f40b5ca957b601a7862e8ac4e57ef60b8dd69
OregTuz00001 <kanyaiamrk2007@gmail.com>
2025-01-10 09:07:26 +0100

    ablakok

--- teszt.py
+++ teszt.py
@@ -4,7 +4,7 @@ from tkinter.ttk import *
 master = Tk()
  
 
-master.geometry("200x200")
+master.geometry("600x600")
  
  
 
@@ -17,23 +17,59 @@ def openNewWindow():
     newWindow.title("New Window")
  
     
-    newWindow.geometry("200x200")
+    newWindow.geometry("400x400")
  
     
     Label(newWindow, 
           text ="This is a new window").pack()
  
+def openNewWindow2():
+     
+    
+    newWindow = Toplevel(master)
  
+    
+    newWindow.title("New Window")
+ 
+    
+    newWindow.geometry("400x400")
+ 
+    
+    Label(newWindow, 
+          text ="This is a new window").pack()
+def openNewWindow3():
+     
+    
+    newWindow = Toplevel(master)
+ 
+    
+    newWindow.title("New Window")
+ 
+    
+    newWindow.geometry("400x400")
+ 
+    
+    Label(newWindow, 
+          text ="This is a new window").pack()
+    
+
 label = Label(master, 
               text ="This is the main window")
  
 label.pack(pady = 10)
  
 
-btn = Button(master, 
+btn1 = Button(master, 
              text ="Click to open a new window", 
              command = openNewWindow)
-btn.pack(pady = 10)
- 
+btn2 = Button(master, 
+             text ="Click to open a new window", 
+             command = openNewWindow2)
+btn3 = Button(master, 
+             text ="Click to open a new window", 
+             command = openNewWindow3)
+btn1.pack(pady = 10)
+btn2.pack(pady = 10)
+btn3.pack(pady = 10)
 
 mainloop()
