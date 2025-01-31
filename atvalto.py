ee49d588b1bd27d2cc2af6c599693c8d04de1c15
BarathBM <bercimiki1@gmail.com>
2025-01-31 09:17:42 +0100

    Debug2

--- atvalto.py
+++ atvalto.py
@@ -14,7 +14,7 @@ def openNewWindow(title):
         try:
             value = float(entry_value.get())  # A bevitt értéket próbáljuk float-ra alakítani
         except ValueError:
-            label_result.config(text="Hiba: Kérem adjon meg egy számot!")  # Hibás input 
+            label_result.config(text="Hiba: Kérem adjon meg egy számot!")  # Hibás input esetén üzenet
             return
         
         # Használjuk a .get() metódust a StringVar-ok értékének lekéréséhez
@@ -79,30 +79,30 @@ def openNewWindow(title):
             else:
                 label_result.config(text="Eredmény: " + str(result)+" cm")
         label_value = Label(newWindow, text="Érték:")
-        label_value.grid(row=0, column=0)
+        label_value.pack(pady=10)
         entry_value = Entry(newWindow)
-        entry_value.grid(row=0, column=1)
+        entry_value.pack(pady=10)
 
         # Kiválasztó menük az egyes mértékegységekhez
         label_from_unit = Label(newWindow, text="Forrás mértékegység:")
-        label_from_unit.grid(row=1, column=0)
+        label_from_unit.pack(pady=10)
         from_unit = StringVar(value="méter")
         dropdown_from = OptionMenu(newWindow, from_unit, "méter", "centiméter", "kilométer")
-        dropdown_from.grid(row=1, column=1)
+        dropdown_from.pack(pady=10)
 
         label_to_unit = Label(newWindow, text="Cél mértékegység:")
-        label_to_unit.grid(row=2, column=0)
+        label_to_unit.pack(pady=10)
         to_unit = StringVar(value="centiméter")
         dropdown_to = OptionMenu(newWindow, to_unit, "méter", "centiméter", "kilométer")
-        dropdown_to.grid(row=2, column=1)
+        dropdown_to.pack(pady=10)
 
         # Átváltás gomb
         button_convert = Button(newWindow, text="Átváltás", command=converthossz)
-        button_convert.grid(row=3, column=0, columnspan=2)
+        button_convert.pack(pady=10)
 
         # Eredmény megjelenítése
         label_result = Label(newWindow, text="")
-        label_result.grid(row=4, column=0, columnspan=2)
+        label_result.pack(pady=10)
     elif(title=="Tömeg"):
         def converttomeg():
             try:
@@ -139,30 +139,30 @@ def openNewWindow(title):
             else:
                 label_result.config(text="Eredmény: " + str(result)+" dkg")
         label_value = Label(newWindow, text="Érték:")
-        label_value.grid(row=0, column=0)
+        label_value.pack(pady=10)
         entry_value = Entry(newWindow)
-        entry_value.grid(row=0, column=1)
+        entry_value.pack(pady=10)
 
         # Kiválasztó menük az egyes mértékegységekhez
         label_from_unit = Label(newWindow, text="Forrás mértékegység:")
-        label_from_unit.grid(row=1, column=0)
+        label_from_unit.pack(pady=10)
         from_unit = StringVar(value="méter")
         dropdown_from = OptionMenu(newWindow, from_unit, "gramm", "dekagramm", "kilogramm")
-        dropdown_from.grid(row=1, column=1)
+        dropdown_from.pack(pady=10)
 
         label_to_unit = Label(newWindow, text="Cél mértékegység:")
-        label_to_unit.grid(row=2, column=0)
+        label_to_unit.pack(pady=10)
         to_unit = StringVar(value="centiméter")
         dropdown_to = OptionMenu(newWindow, to_unit, "gramm", "dekagramm", "kilogramm")
-        dropdown_to.grid(row=2, column=1)
+        dropdown_to.pack(pady=10)
 
         # Átváltás gomb
         button_convert = Button(newWindow, text="Átváltás", command=converttomeg)
-        button_convert.grid(row=3, column=0, columnspan=2)
+        button_convert.pack(pady=10)
 
         # Eredmény megjelenítése
         label_result = Label(newWindow, text="")
-        label_result.grid(row=4, column=0, columnspan=2)
+        label_result.pack(pady=10)
     else:
         def convertido():
             try:
@@ -199,41 +199,38 @@ def openNewWindow(title):
             else:
                 label_result.config(text="Eredmény: " + str(result)+" óra")
         label_value = Label(newWindow, text="Érték:")
-        label_value.grid(row=0, column=0)
+        label_value.pack(pady=10)
         entry_value = Entry(newWindow)
-        entry_value.grid(row=0, column=1)
+        entry_value.pack(pady=10)
 
         # Kiválasztó menük az egyes mértékegységekhez
         label_from_unit = Label(newWindow, text="Forrás mértékegység:")
-        label_from_unit.grid(row=1, column=0)
+        label_from_unit.pack(pady=10)
         from_unit = StringVar(value="méter")
         dropdown_from = OptionMenu(newWindow, from_unit, "másodperc", "perc", "óra")
-        dropdown_from.grid(row=1, column=1)
+        dropdown_from.pack(pady=10)
 
         label_to_unit = Label(newWindow, text="Cél mértékegység:")
-        label_to_unit.grid(row=2, column=0)
+        label_to_unit.pack(pady=10)
         to_unit = StringVar(value="centiméter")
         dropdown_to = OptionMenu(newWindow, to_unit, "másodperc", "perc", "óra")
-        dropdown_to.grid(row=2, column=1)
+        dropdown_to.pack(pady=10)
 
         # Átváltás gomb
         button_convert = Button(newWindow, text="Átváltás", command=convertido)
-        button_convert.grid(row=3, column=0, columnspan=2)
+        button_convert.pack(pady=10)
 
         # Eredmény megjelenítése
         label_result = Label(newWindow, text="")
-        label_result.grid(row=4, column=0, columnspan=2) 
+        label_result.pack(pady=10) 
     label = Label(root, text="Főablak")
-    label.grid(row=0, column=0, pady=10)
+    label.pack(pady=10)
 
 btn1 = Button(root, text="Tömeg átváltás", command=lambda: openNewWindow("Tömeg"))
 btn2 = Button(root, text="Hossz átváltás", command=lambda: openNewWindow("Hossz"))
 btn3 = Button(root, text="Idő átváltás", command=lambda: openNewWindow("Idő"))
-btn1.grid(row=1, column=1, pady=10)
-
-btn2.grid(row=2, column=1, pady=10)
-
-btn3.grid(row=3, column=1, pady=10)
-
+btn1.pack(pady=10)
+btn2.pack(pady=10)
+btn3.pack(pady=10)
 
 mainloop()
